e1a367a6adb45b451c29ba07c09ff3bc15f2d790
Qurat62 <quratulainaftab62@gmail.com>
2021-05-25 04:15:30 -0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,6 @@
+//next is a framworkf for react just to build a layer on react . serside rendering
+module.exports= {
+    images:{
+        domains:["links.papareact.com","fakestoreapi.com"],    
+    },
+};
